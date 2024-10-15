06a215bf9f75e2a417591eda7e5d1c4d66ec68a1
danielpf09 <estebanperexx@gmail.com>
2024-10-15 11:33:08 -0500

    Ajuste de la capa dominio

--- src/lib/User/domain/UserRepository.ts
+++ src/lib/User/domain/UserRepository.ts
@@ -2,8 +2,8 @@ import { User } from "./User";
 import { UserId } from "./UserId";
 
 export interface UserRepository {
-  create(user: User): Promise<void>;
-  getAll(): Promise<User[]>;
+  create(user: User): Promise<void>; 
+  getAll(): Promise<User[]>;  /*Me trae un array de usuarios de nuestro dominio*/ 
   getOneById(id: UserId): Promise<User | null>;
   edit(user: User): Promise<void>;
   delete(id: UserId): Promise<void>;
